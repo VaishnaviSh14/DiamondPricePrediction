435bf993c00efffcc2c01fc07f2ba58684d393ec
VaishnaviSh14 <sharmavaishnavi1403@gmail.com>
2023-09-22 22:56:24 +0530

    ML

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        return requirements
+    
+        
+setup(
+    name='DiamondPricePredicition',
+    version='0.0.1',
+    author='Vaishnavi',
+    author_email='sharmavaishnavi1403@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
